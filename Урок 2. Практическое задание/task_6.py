d5532805c26919396871d3695df16a44548ddef6
Vasiliy Andreev <93121991+MyNameIs-Witcher@users.noreply.github.com>
2022-10-16 22:02:54 +0300

    выполнение task_6

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,27 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+from random import randint
+
+def find_answer(quest=randint(0,101), ticker=0):
+  if ticker < 10:
+    user_answer = int(input('Попробуйте ввести число: '))
+    if user_answer == quest:
+      print('You win!')
+      return
+    elif user_answer < quest:
+      print('Загаданное число больше')
+    else:
+      print('Загаданное число меньше')
+    ticker += 1
+    find_answer(quest, ticker)
+  else:
+    print(f'You Lose! Find answer is {quest}')
+    return  
+
+
+if __name__ == '__main__':
+  find_answer()
+
+  
